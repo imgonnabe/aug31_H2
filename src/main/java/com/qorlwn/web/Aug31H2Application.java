e2f6dd276fd20a02efb4b2ece100d2455de185df
imgonnabe <qorlwn456123@gmail.com>
2023-08-31 10:44:09 +0900

    aug31_H2

---
+++ src/main/java/com/qorlwn/web/Aug31H2Application.java
@@ -0,0 +1,13 @@
+package com.qorlwn.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aug31H2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aug31H2Application.class, args);
+	}
+
+}
